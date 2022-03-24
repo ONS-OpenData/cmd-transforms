f2507f7c732efd0e0a06f501fe3271d5f59bd308
James Bryant <jamesbryant792@gmail.com>
2022-03-24 12:13:09 +0000

    update main.py

--- lambda-test/main.py
+++ lambda-test/main.py
@@ -7,8 +7,15 @@ def transform(file):
     """
 
     df = pd.read_csv(file, dtype=str)
-    columns = list(df.columns)
-    print(f"columns from function - {columns}")
-    return columns
+    print(f"columns from function - {df.columns}")
+
+    df = df[[
+        'v4_1', 'Data Marking', 'calendar-years', 'Time', 'uk-only',
+        'Geography', 'adzuna-jobs-category', 'AdzunaJobsCategory',
+        'week-number', 'Week'
+        ]]
+    print(f"columns from function after transform - {df.columns}")
+    
+    return list(df.columns)
         
 
