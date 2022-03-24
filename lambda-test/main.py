bd55ae183a549f64638e3cb0a0efea6fea41ea21
James Bryant <jamesbryant792@gmail.com>
2022-03-24 12:33:21 +0000

    missing code

--- lambda-test/main.py
+++ lambda-test/main.py
@@ -7,8 +7,10 @@ def transform(files):
     """
     assert type(files) == list, f"transform takes in a list, not {type(files)}"
     assert len(files) == 1, f"transform only takes in 1 source file, not {len(files)}"
-
+    file = files[0]
     output_file = "/tmp/v4-cpih.csv"
+    
+    df = pd.read_csv(file, dtype=str)
 
     df['Time'] = df['time']
 
