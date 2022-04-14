ddb1bdc2e6d9e6bb7a8ef4a2f7c58159ca470f17
James Bryant <jamesbryant792@gmail.com>
2022-04-14 13:49:48 +0100

    suicides transform

---
+++ suicides/main.py
@@ -0,0 +1,64 @@
+from databaker.framework import *
+import pandas as pd
+
+def transform(files):
+    assert type(files) == list, f"transform takes in a list, not {type(files)}"
+    assert len(files) == 1, f"transform only takes in 1 source file, not {len(files)}"
+    file = files[0]
+    output_file = "/tmp/v4-suicides-in-the-uk.csv"
+
+    tabs = loadxlstabs(file, ["Table 1"])
+
+    """DataBaking"""
+    conversionsegments = []
+    for tab in tabs:
+        
+        junk = tab.excel_ref("A4").expand(DOWN).filter(contains_string("Footnote")).expand(DOWN).expand(RIGHT)
+        time = tab.excel_ref("E6").expand(RIGHT).is_not_blank().is_not_whitespace()
+        geog = tab.excel_ref("A8").expand(DOWN).is_not_blank().is_not_whitespace()
+        geog -= junk
+        
+        obs = geog.waffle(time)
+        
+        dimensions = [
+                HDim(time, TIME, DIRECTLY, ABOVE),
+                HDim(geog, GEOG, DIRECTLY, LEFT)
+                ]
+        
+        conversionsegment = ConversionSegment(tab, dimensions, obs).topandas()
+        conversionsegments.append(conversionsegment)
+        
+    df = pd.concat(conversionsegments)
+
+    '''Post processing'''
+    df["V4_0"] = df["OBS"].apply(v4Integers)
+    df["Time"] = df["TIME"]
+    df = df[df["GEOG"] != "J99000001"]
+
+    df = df.rename(columns={
+            "OBS": "v4_0",
+            "TIME": "calendar-years",
+            "GEOG": "administrative-geography",
+            "geog_labels": "Geography"
+            }
+        )
+
+    df = df[[
+            "v4_0", "calendar-years", "Time", "administrative-geography", "Geography"
+            ]]
+
+    df.to_csv(output_file, index=False)
+    print("Transform Complete")
+    return output_file
+
+
+def v4Integers(value):
+    """
+    treats all values in v4 column as strings
+    returns integers instead of floats for numbers ending in ".0"
+    """
+    new_value = str(value)
+    if new_value.endswith(".0"):
+        new_value = new_value[:-2]
+    return new_value
+        
